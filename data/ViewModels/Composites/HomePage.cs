678b5eeabae862feaf0635f5d88e776c71f5390a
robert.dondo <robert.dondo@dstvdm.com>
2022-04-28 18:40:33 +0200

    Refactor Homepage model

--- data/ViewModels/Composites/HomePage.cs
+++ data/ViewModels/Composites/HomePage.cs
@@ -1,25 +1,26 @@
 ﻿using System.Collections.Generic;
 using Curate.Data.Models;
 using Curate.Data.ViewModels.Blogs;
-using Curate.Data.ViewModels.News;
 using Curate.Data.ViewModels.Podcasts;
 
 namespace Curate.Data.ViewModels.Composites
 {
     public class HomePage
     {
-        public List<MinimalArticle> BreakingNews { get; set; }
-        public List<MinimalArticle> PopularNews { get; set; }
-
-        public List<MinimalArticle> FeaturedNews { get; set; }
 
         public List<Video> FeaturedVideos { get; set; }
 
-        public List<Post> LatestNyambo { get; set; }
+        public List<Post> FeaturedNyambo { get; set; }
 
         public List<Blog> FeaturedBlogs { get; set; }
 
         public List<Podcast> FeaturedPodcasts { get; set; }
+
+        public List<VideoChannel> FeaturedBchannels { get; set; }
+
+        public List<Tag> FeaturedTags { get; set; }
+
+        public List<VideoPlaylist> FeaturedBplaylist { get; set; }
 
         /* TODO
          
